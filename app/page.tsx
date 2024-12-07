7ea917b16cb1d63b0f010fb71df0631db56bb3b4
aoto0218 <ktc24a31g0010@edu.kyoto-tech.ac.jp>
2024-12-07 12:37:56 +0900

    no67

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-  import { title } from "process";
-import Styles from "./page.module.css";
+import styles from "./page.module.css";
 import Image from "next/image";
+
+import ButtonLink from "./_components/ButtonLink";
 
 type News = {
   id: string;
@@ -12,7 +13,7 @@ type News = {
   createdAt: string;
 };
 
-const date: {
+const data: {
   contents: News[] } = {
     contents: [
       {
@@ -30,6 +31,10 @@ const date: {
         category: {
           name: "更新情報",
         },
+        publishedAt: "2023/05/19",
+        createdAt: "2023/05/19",
+        },
+        {
         id: "3",
         title: "テストの記事です",
         category: {
@@ -42,46 +47,46 @@ const date: {
   };  
 
 export default function Home() {
-  const sliceDate = date.contents.slice(0, 2);
+  const sliceData = data.contents.slice(0, 2);
+  
   return (
     <>
-    <section className={Styles.top}>
+    <section className={styles.top}>
   <div>
-    <h1 className={Styles.title}>テクノロジーの力で世界を変える</h1>
-    <p className={Styles.description}>
+    <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
+    <p className={styles.description}>
       私たちは市場をリードしているグローバルテックカンパニーです。
     </p>
    </div>
    <Image
-     className={Styles.bgimg}
+     className={styles.bgimg}
      src="/img-mv.jpg"
      alt=""
      width={4000}
      height={1200}
      />
    </section>
-   <section className={Styles.news}>
+   <section className={styles.news}>
 
-    <h2 className={Styles.newsTitle}>News</h2>
+    <h2 className={styles.newsTitle}>News</h2>
     <ul>
-      {sliceDate.map((article) =>
-      {date.contents.map((article) => (
-        <li key={article.id} className={Styles.list}>
-          <div className={Styles.link}>
+      {sliceData.map((article) => (
+        <li key={article.id} className={styles.list}>
+          <div className={styles.link}>
             <Image
-              className={Styles.image}
+              className={styles.image}
               src="/no-imge.png"
               alt="No Image"
               width={1200}
               height={630}
               />
-            <dl className={Styles.content}>
-              <dt className={Styles.newsItemTitle}>
+            <dl className={styles.content}>
+              <dt className={styles.newsItemTitle}>
                 {article.title}
               </dt>
-              <dd className={Styles.date}>
-                <span className={Styles.tag}>{article.category.name}</span>
-                <span className={Styles.date}>
+              <dd className={styles.date}>
+                <span className={styles.tag}>{article.category.name}</span>
+                <span className={styles.date}>
                   <Image
                   src="/clock.svg"
                   alt=""
@@ -97,6 +102,9 @@ export default function Home() {
           </li>
         ))}
         </ul>
+        <div className={styles.newsLink}>
+          <ButtonLink href="/news">もっとみる</ButtonLink>
+        </div>  
       </section>
     </>
   );
