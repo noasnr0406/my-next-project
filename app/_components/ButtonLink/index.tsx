c48e5e5ad65f1a123ff0fd3928460d8b1f8dc0d2
aoto0218 <ktc24a31g0010@edu.kyoto-tech.ac.jp>
2024-12-21 12:38:30 +0900

    のno.73

--- app/_components/ButtonLink/index.tsx
+++ app/_components/ButtonLink/index.tsx
@@ -1,5 +1,14 @@
-import styles from "./index.module.css";
+import styles from "./index.module.css"; 
 
 type Props = {
     href: string;
-    
+    children: React.ReactNode;
+};
+
+export default function ButtonLink({href, children}:Props) {
+　return (
+    <a href={href} className={styles.button}>
+       {children}
+    </a>                                                                                                                                                                                                                                                                                                                                                                  rm5nt5　　　　　
+  );
+} 
